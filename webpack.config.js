1e92f7b8045eb32b3717354e5f42ced863ef8b01
Daf <dafne.azzolina@gmail.com>
2022-05-31 15:55:34 -0300

    Change port from 8080 to 35145 for lighthouse

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
   devServer: {
+    port:'35145',
     static: './dist',
   },
   plugins: [
