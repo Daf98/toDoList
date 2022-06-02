f8862b600fe7c7ad4ec74812ca32eae74e198212
Daf <dafne.azzolina@gmail.com>
2022-06-02 18:22:53 -0300

    Add edit function

--- src/index.js
+++ src/index.js
@@ -26,35 +26,41 @@ class ListItem {
 const itemArray = [];
 //Add new item
 const addNewItem = (newDescription) => {
-const newItem = document.createElement('div');
-newItem.classList.add('newItem');
-newItem.innerHTML += `
+  const newItem = document.createElement('div');
+  newItem.classList.add('new-item');
+  newItem.innerHTML = `
 <input type="checkbox" class="checkbox">
 <span>${newDescription}</span>
 <i class="fa-solid fa-ellipsis-vertical"></i>
 <i class="fa-solid fa-trash-can"></i>
 `
-listOfItems.appendChild(newItem);
-// make checkbox work
-const checkbox = document.querySelectorAll('.checkbox');
-checkbox.forEach(checkboxInput => {
-checkboxInput.addEventListener('click', () => {
-  checkboxInput.parentNode.classList.toggle('strike');
-  const ellipsisIcon = checkboxInput.parentNode.childNodes[5];
-  ellipsisIcon.classList.toggle('inactive-ellipsis');
-  const trashIcon = checkboxInput.parentNode.childNodes[8];
-  trashIcon.classList.toggle('active-trash');
-});
-//Create and send new item to local storage
-const newListItem = new ListItem(newDescription, false, '0');
-itemArray.push(newListItem);
-console.log(itemArray);
-const stringedItems = JSON.stringify(itemArray);
-localStorage.setItem('items', stringedItems);
-});
+  listOfItems.appendChild(newItem);
+  // make checkbox work
+  const checkbox = document.querySelectorAll('.checkbox');
+  checkbox.forEach(checkboxInput => {
+    checkboxInput.addEventListener('click', () => {
+      checkboxInput.parentNode.classList.toggle('strike');
+      const ellipsisIcon = checkboxInput.parentNode.childNodes[5];
+      ellipsisIcon.classList.toggle('inactive-ellipsis');
+      const trashIcon = checkboxInput.parentNode.childNodes[8];
+      trashIcon.classList.toggle('active-trash');
+    });
+  });
+  //Create and send new item to local storage
+  const newListItem = new ListItem(newDescription, false, checkbox.length - 1);
+  itemArray.push(newListItem);
+  const stringedItems = JSON.stringify(itemArray);
+  localStorage.setItem('items', stringedItems);
+  //Edit items
+  const editItem = document.querySelectorAll('.fa-ellipsis-vertical');
+  editItem.forEach((item) => { //editIcons
+    item.addEventListener("click", () => {
+      item.parentNode.classList.toggle('clicked-on');
+      editItems(item.previousElementSibling, newItem); //editTodo
+    });
+  })
 
 }
-
 //Add event listener to input field
 mainInput.addEventListener('keypress', (e) => {
   if (e.key === 'Enter' && mainInput.value) {
@@ -63,3 +69,27 @@ mainInput.addEventListener('keypress', (e) => {
     mainInput.value = '';
   }
 });
+
+//Edit items
+const editItems = (newItem, editedContainer) => { //editTodo
+  const newInput = document.createElement('input'); //editInput
+  newInput.type = 'text';
+  newInput.classList.add('new-input');
+  newInput.value = newItem.textContent;
+  editedContainer.replaceChild(newInput, newItem); //The node to be replaced is not a child of this node.
+  newInput.addEventListener("keypress", (e) => {
+    if (e.key === 'Enter') {
+      const editedContainers = document.querySelectorAll('.new-item');
+      const parsedItems = localStorage.getItem('items');
+      const localData = JSON.parse(parsedItems);
+      //fetch item based on class 'strike'
+      for (let i = 0; i < editedContainers.length; i++) {
+        if (editedContainers[i].classList.contains('clicked-on')) {
+          localData[i].description = newInput.value;
+          const stringedData = JSON.stringify(localData);
+          localStorage.setItem('items', stringedData);
+        }
+      }
+    }
+  })
+}
