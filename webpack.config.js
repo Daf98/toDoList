f9403522b3ea29674612328e9a839dcb521834c7
Daf <dafne.azzolina@gmail.com>
2022-06-07 13:50:44 -0300

    Add public path

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
+    publicPath: '/toDoList/',
   },
   module: {
     rules: [
